848d63ed7a1eac145b999049ff1571a4593029a0
czt740277638 <740277638@qq.com>
2017-01-23 17:58:58 +0800

    thisi is

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("this is test....");
+        print("this is test....");
+        print("this is test....");
+        print("this is test....");
+        print("this is test....");
+        
     }
 
     override func didReceiveMemoryWarning() {
